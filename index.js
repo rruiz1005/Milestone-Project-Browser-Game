58c046109f59192a38ab33e4c02750b34d5ceda0
Ryan Ruiz <ryan.hans1176@gmail.com>
2022-04-10 17:29:14 -0400

    added player two to function

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentPlayer = 1
 
 
+
+
     for (let i = 0; i < squares.length; i++) {
         squares[i].onclick = () => {
             //if the square below your current square is taken, you can go on top of it
@@ -15,7 +17,15 @@ document.addEventListener('DOMContentLoaded', () => {
                     currentPlayer = 2
                     displayCurrentPlayer.innerHTML = currentPlayer
                 }
-            }
+                else if (currentPlayer == 2) {
+                    squares[i].classList.add('taken')
+                    squares[i].classList.add('player-two')
+                    currentPlayer = 1
+                    displayCurrentPlayer.innerHTML = currentPlayer
+                }
+                
+            } else alert ('cant go here')
+            checkBoard()
         }
     }
 
